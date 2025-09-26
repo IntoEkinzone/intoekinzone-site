bb41d3148fb5ff9531456f6bd261cdad5b7aec79
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-09-27 00:55:49 +0200

    remove unused import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://archive.ekin.zone',
